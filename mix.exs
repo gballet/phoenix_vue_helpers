0a1bd46c897d6dae689f55422fb40c5e7c1fd259
Guillaume Ballet <gballet@gmail.com>
2017-06-17 08:23:17 +0000

    [fix] Typo preventing compilation as a dependency

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule PhoenixVueHelpers.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :phoenix_vue,
+    [app: :phoenix_vue_helpers,
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
